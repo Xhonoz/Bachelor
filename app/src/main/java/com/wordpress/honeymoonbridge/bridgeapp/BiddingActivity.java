27e86f679ab35a67ed25953b70b88f9aead6b3ae
Markus <markus.loland@hotmail.com>
2018-03-18 13:29:52 +0100

    Added a new whellPicker biddingMenu

--- app/src/main/java/com/wordpress/honeymoonbridge/bridgeapp/BiddingActivity.java
+++ app/src/main/java/com/wordpress/honeymoonbridge/bridgeapp/BiddingActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.NumberPicker;
 import android.widget.Toast;
 
 public class BiddingActivity extends AppCompatActivity {
@@ -13,7 +14,24 @@ public class BiddingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bidding);
+        setContentView(R.layout.activity_bidding2);
+
+        NumberPicker numberPicker1 = findViewById(R.id.np1);
+        numberPicker1.setMaxValue(7);
+        numberPicker1.setMinValue(1);
+        numberPicker1.setWrapSelectorWheel(true);
+        numberPicker1.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
+        NumberPicker numberPicker2 = findViewById(R.id.np2);
+        numberPicker2.setMaxValue(5);
+        numberPicker2.setMinValue(1);
+        numberPicker2.setWrapSelectorWheel(true);
+        numberPicker2.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
+
+
+
+        numberPicker2.setDisplayedValues(new String[]{"♣", "♦", "♥", "♠", "NT"});
+
+
 
     }
 
