fe80ca47ee42e96d440157f6fe4bfb65c8089573
Carmen <cji96@hotmail.com>
2018-04-09 14:24:17 +0200

    Added a card and suit class

---
+++ app/src/main/java/com/wordpress/honeymoonbridge/bridgeapp/Model/Card.java
@@ -0,0 +1,10 @@
+package com.wordpress.honeymoonbridge.bridgeapp.Model;
+
+/**
+ * Created by Carmen on 09.04.2018.
+ */
+
+public class Card {
+    private Suit suit;
+    private int cardValue;
+}
---
+++ app/src/main/java/com/wordpress/honeymoonbridge/bridgeapp/Model/Suit.java
@@ -0,0 +1,9 @@
+package com.wordpress.honeymoonbridge.bridgeapp.Model;
+
+/**
+ * Created by Carmen on 09.04.2018.
+ */
+
+public enum Suit {
+    Clubs, Diamonds, Hearts, Spades;
+}
