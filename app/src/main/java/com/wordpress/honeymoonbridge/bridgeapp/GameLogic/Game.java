72c591a5829dc65bd8602cfbc90963496ffbaf8f
Markus <markus.loland@hotmail.com>
2018-04-10 11:13:20 +0200

    added constructor and some other methods to game

--- app/src/main/java/com/wordpress/honeymoonbridge/bridgeapp/GameLogic/Game.java
+++ app/src/main/java/com/wordpress/honeymoonbridge/bridgeapp/GameLogic/Game.java
@@ -11,7 +11,12 @@ public class Game {
 private GameState gamestate;
 private AIPlayer AI;
 
-public Card getTopCard(){
+public Game game(){
+    gamestate = new GameState(true);
+
+}
+
+public Card peakTopCard(){
     CardStack s = gamestate.getStack();
     if(!gamestate.getStack().isEmpty())
         return (Card)s.get(0);
@@ -19,10 +24,38 @@ public Card getTopCard(){
     return null;
 }
 
-//public void PickCard(Player player, boolean first){
-//    if(player == Player.NORTH)
-//
-//}
+    public Card popTopCard(){
+        CardStack s = gamestate.getStack();
+        if(!gamestate.getStack().isEmpty()) {
+            Card c = (Card) s.get(0);
+            s.remove(c);
+            return c;
+        }
+
+        return null;
+    }
+
+public void PickCard(Player player, boolean first){
+
+        Card fi = getAndRemoveTopCard();
+        Card se = getAndRemoveTopCard();
+    if(player == Player.NORTH) {
+        gamestate.getNorth26Cards().add(fi);
+        gamestate.getNorth26Cards().add(se);
+        if (first)
+            gamestate.getNorthHand().add(fi);
+        else
+            gamestate.getNorthHand().add(se);
+    }
+    if(player == Player.SOUTH) {
+        gamestate.getSouth26Cards().add(fi);
+        gamestate.getSouth26Cards().add(se);
+        if (first)
+            gamestate.getSouthHand().add(fi);
+        else
+            gamestate.getSouthHand().add(se);
+    }
+}
 
 
 
