ff0a178e6b0c98c17def2ca6a294c8722ad99d33
Markus <markus.loland@hotmail.com>
2018-04-10 11:00:19 +0200

    added getTopCard

--- app/src/main/java/com/wordpress/honeymoonbridge/bridgeapp/GameLogic/Game.java
+++ app/src/main/java/com/wordpress/honeymoonbridge/bridgeapp/GameLogic/Game.java
@@ -1,5 +1,8 @@
 package com.wordpress.honeymoonbridge.bridgeapp.GameLogic;
 
+import com.wordpress.honeymoonbridge.bridgeapp.Model.Card;
+import com.wordpress.honeymoonbridge.bridgeapp.Model.CardStack;
+
 /**
  * Created by Eier on 09.04.2018.
  */
@@ -9,8 +12,19 @@ private GameState gamestate;
 private AIPlayer AI;
 
 public Card getTopCard(){
+    CardStack s = gamestate.getStack();
+    if(!gamestate.getStack().isEmpty())
+        return (Card)s.get(0);
 
+    return null;
 }
+
+//public void PickCard(Player player, boolean first){
+//    if(player == Player.NORTH)
+//
+//}
+
+
 
 
 
