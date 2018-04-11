a8e5f9565c752988f89aaf807560863d2dd97d12
Carmen <cji96@hotmail.com>
2018-04-11 13:54:09 +0200

    Something got deleted

--- app/src/main/java/com/wordpress/honeymoonbridge/bridgeapp/GameLogic/Game.java
+++ app/src/main/java/com/wordpress/honeymoonbridge/bridgeapp/GameLogic/Game.java
@@ -8,21 +8,21 @@ import com.wordpress.honeymoonbridge.bridgeapp.Model.CardStack;
  */
 
 public class Game {
-private GameState gamestate;
-private AIPlayer AI;
+    private GameState gamestate;
+    private AIPlayer AI;
 
+    public Game(){
+        gamestate = new GameState(true);
 
-public Game(boolean isSouthTurn){
-    gamestate = new GameState(true);
-}
+    }
 
-public Card peakTopCard(){
-    CardStack s = gamestate.getStack();
-    if(!gamestate.getStack().isEmpty())
-        return (Card)s.get(0);
+    public Card peakTopCard(){
+        CardStack s = gamestate.getStack();
+        if(!gamestate.getStack().isEmpty())
+            return (Card)s.get(0);
 
-    return null;
-}
+        return null;
+    }
 
     public Card popTopCard(){
         CardStack s = gamestate.getStack();
@@ -35,40 +35,49 @@ public Card peakTopCard(){
         return null;
     }
 
-private void PickCard(Player player, boolean first){
+    public void UIPickCard(boolean first){
+        if(gamestate.getPhase().equals(Phase.PICKING) && gamestate.isSouthTurn()){
+            PickCard(Player.SOUTH, first);
+            AITakesTurn();
+        }
 
-        Card fi = popTopCard();
-        Card se = popTopCard();
-    if(player == Player.NORTH) {
-        gamestate.getNorth26Cards().add(fi);
-        gamestate.getNorth26Cards().add(se);
-        gamestate.getNorthChoseFirst().add(first);
-        if (first)
-            gamestate.getNorthHand().add(fi);
-        else
-            gamestate.getNorthHand().add(se);
     }
-    if(player == Player.SOUTH) {
-        gamestate.getSouth26Cards().add(fi);
-        gamestate.getSouth26Cards().add(se);
-        gamestate.getSouthChoseFirst().add(first);
 
-        if (first)
-            gamestate.getSouthHand().add(fi);
-        else
-            gamestate.getSouthHand().add(se);
+    public void PickCard(Player player, boolean first){
+        if(!gamestate.getStack().isEmpty()) {
+            Card fi = popTopCard();
+            Card se = popTopCard();
+            if (player == Player.NORTH) {
+                gamestate.getNorth26Cards().add(fi);
+                gamestate.getNorth26Cards().add(se);
+                gamestate.getNorthChoseFirst().add(first);
+                if (first)
+                    gamestate.getNorthHand().add(fi);
+                else
+                    gamestate.getNorthHand().add(se);
+            }
+            if (player == Player.SOUTH) {
+                gamestate.getSouth26Cards().add(fi);
+                gamestate.getSouth26Cards().add(se);
+                gamestate.getSouthChoseFirst().add(first);
+                if (first)
+                    gamestate.getSouthHand().add(fi);
+                else
+                    gamestate.getSouthHand().add(se);
+            }
+        } else{
+            //Husk å endre hvis spilleren har huket av i settings at bidding ikke skal
+            //være med da går vi rett til spille fasen
+            gamestate.setPhase(Phase.BIDDING);
+        }
     }
-}
 
-public GameState getGameState(){
-    return gamestate;
-}
+    public GameState getGameState() {
+        return gamestate;
+    }
 
-
-
-
-
-
-
-
+    private void AITakesTurn(){
+        boolean first = AI.pickCard(gamestate);
+        PickCard(Player.SOUTH,first);
+    }
 }
