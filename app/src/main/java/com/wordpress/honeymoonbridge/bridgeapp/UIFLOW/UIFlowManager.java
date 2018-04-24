248f72809345ada181da075676c9c06fd59974fe
Markus <markus.loland@hotmail.com>
2018-04-24 11:17:34 +0200

    UIFLOW

---
+++ app/src/main/java/com/wordpress/honeymoonbridge/bridgeapp/UIFLOW/UIFlowManager.java
@@ -0,0 +1,24 @@
+package com.wordpress.honeymoonbridge.bridgeapp.UIFLOW;
+
+import android.app.Fragment;
+
+import com.wordpress.honeymoonbridge.bridgeapp.HandLayout.HandAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Eier on 24.04.2018.
+ */
+
+public class UIFlowManager {
+
+    ArrayList<UIAction> actions;
+
+    public boolean next(HandAdapter southHand, Fragment gameFragment){
+        switch(actions.remove(0)){
+            case UIAction.GetNewCards_PICKING:
+
+                break;
+        }
+    }
+}
